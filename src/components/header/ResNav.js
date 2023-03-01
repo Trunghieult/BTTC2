098b3c5ca192cf7a3f69bbc101d92e1f8ff667bc
hieu.ltt <hieultt.223@gmail.com>
2023-03-01 18:05:16 +0700

    first commit

---
+++ src/components/header/ResNav.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./ResNav.css";
+import ScrollEvent from "../../function/ScrollEvent";
+
+const ResNav = (props) => {
+  const { showReversePage } = props;
+  return (
+    <div className="res-nav">
+      <Link to="/Menu" style={{ color: "#d2ae68", margin: " 0 auto" }}>
+        Menu
+      </Link>
+      <Link to="/Promotion" style={{ color: "#d2ae68", margin: " 0 auto" }}>
+        Promo
+      </Link>
+      <Link to="/AboutUs" style={{ color: "#d2ae68", margin: " 0 auto" }}>
+        About Us
+      </Link>
+      <div
+        onClick={showReversePage}
+        style={{ color: "#d2ae68", margin: " 0 auto" }}
+        className="res-reverse"
+      >
+        Reservation
+      </div>
+    </div>
+  );
+};
+
+export default ResNav;
